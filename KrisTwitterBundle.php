505eae289258015804db634e734c17af3646f97c
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-07-13 09:07:14 -0700

    Updated buildContainer() signature.

--- KrisTwitterBundle.php
+++ KrisTwitterBundle.php
@@ -3,13 +3,13 @@
 namespace Bundle\Kris\TwitterBundle;
 
 use Symfony\Framework\Bundle\Bundle;
-use Symfony\Components\DependencyInjection\ContainerInterface;
+use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Components\DependencyInjection\Loader\Loader;
 use Bundle\Kris\TwitterBundle\DependencyInjection\TwitterExtension;
 
 class KrisTwitterBundle extends Bundle
 {
-    public function buildContainer(ContainerInterface $container)
+    public function buildContainer(ParameterBagInterface $parameterBag)
     {
         Loader::registerExtension(new TwitterExtension());
     }
