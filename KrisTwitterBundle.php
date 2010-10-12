df1f150ab8e8a8ba6ceacc5038870ece51e36518
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-10-12 05:07:31 +0200

    Removed unneeded code.

--- KrisTwitterBundle.php
+++ KrisTwitterBundle.php
@@ -3,14 +3,7 @@
 namespace Bundle\Kris\TwitterBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Bundle\Kris\TwitterBundle\DependencyInjection\TwitterExtension;
 
 class KrisTwitterBundle extends Bundle
 {
-    public function buildContainer(ParameterBagInterface $parameterBag)
-    {
-        ContainerBuilder::registerExtension(new TwitterExtension());
-    }
 }
